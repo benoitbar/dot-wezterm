5348ea84e262952a47161793237253b51679c897
Benoit Bar <benoit.bar@gandi.net>
2026-01-12 16:27:05 +0100

    Do not open URLs on mouse click

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -28,6 +28,27 @@ config.keys = {
   },
 }
 
+config.mouse_bindings = {
+  -- Disable the default click behavior
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "NONE",
+    action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
+  },
+  -- Ctrl-click will open the link under the mouse cursor
+  {
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = wezterm.action.OpenLinkAtMouseCursor,
+  },
+  -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+  {
+    event = { Down = { streak = 1, button = "Left" } },
+    mods = "CTRL",
+    action = wezterm.action.Nop,
+  },
+}
+
 local local_config = {}
 if mytable.isModuleAvailable("cfg_local") then
   local_config = require("cfg_local")
